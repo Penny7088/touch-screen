6dc436000aa096ab0e018bce9d3dd29acdd2b9cd
583708076@qq.com <583708076@qq.com>
2017-05-31 17:10:25 +0800

    addErrorMsg

---
+++ ECAndroid/src/main/java/com/yysp/ecandroid/data/response/AddErrorMsgResponse.java
@@ -0,0 +1,86 @@
+package com.yysp.ecandroid.data.response;
+
+/**
+ * Created by Administrator on 2017/5/27.
+ */
+
+public class AddErrorMsgResponse {
+
+
+    private String createTime;
+    private int currentPage;
+    private String id;
+    private String message;
+    private int pageSize;
+    private String resourceId;
+    private String resourceName;
+    private int type;
+
+    public AddErrorMsgResponse(String msg) {
+        this.message=msg;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public int getCurrentPage() {
+        return currentPage;
+    }
+
+    public void setCurrentPage(int currentPage) {
+        this.currentPage = currentPage;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public String getResourceId() {
+        return resourceId;
+    }
+
+    public void setResourceId(String resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public void setResourceName(String resourceName) {
+        this.resourceName = resourceName;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+}
